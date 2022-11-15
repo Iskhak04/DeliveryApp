e7a5ce13f26b8ba8fb33b53d5b5cc85681a2b71d
Iskhak <izhtnv04@gmail.com>
2022-11-15 19:44:56 +0600

    add sign in view

---
+++ DeliveryApp/SignInView.swift
@@ -0,0 +1,15 @@
+//
+//  SignInView.swift
+//  DeliveryApp
+//
+//  Created by Iskhak Zhutanov on 15/11/22.
+//
+
+import UIKit
+
+class SignInView: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
