53eab1289fe9165951651a670004df6571b7febf
Iskhak <izhtnv04@gmail.com>
2022-11-15 19:29:00 +0600

    initial app

---
+++ DeliveryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeliveryApp
+//
+//  Created by Iskhak Zhutanov on 15/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
