73275e207ed205a06f1662cb4d827707ca1d5bbe
Iskhak <izhtnv04@gmail.com>
2022-11-19 10:19:51 +0600

    initial commit

---
+++ DeliveryApp/Views/TabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarController.swift
+//  DeliveryApp
+//
+//  Created by Iskhak Zhutanov on 19/11/22.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
